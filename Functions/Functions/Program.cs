94a398304357b1dc38f61ccb428a9d3b17266f86
Tim Haakman <tim.haakman@gmail.com>
2025-11-18 13:01:02 +0100

    Functions02

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Functions
+﻿using System.Data;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Functions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program Program = new Program(); //variable program
+            Program.Run();
         }
+
+        //hier komt de functie
+        internal void Run()
+        {
+            Console.WriteLine("Dit is nu de start van mijn programma");
+        }
+        
     }
+
 }
